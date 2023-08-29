55a8e53617396da5ee395b9761ed9eb85e398748
Parker <aparkerdf@protonmail.com>
2023-08-29 13:59:30 -0400

    Clon DB conaf

--- backend/app/models/database.py
+++ backend/app/models/database.py
@@ -10,8 +10,17 @@ DB_NAME = os.getenv("DB_NAME")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_ADDRESS = os.getenv("DB_ADDRESS")
 
+DB_CONAF_NAME = os.getenv("DB_CONAF_NAME")
+DB_CONAF_PASSWORD = os.getenv("DB_CONAF_PASSWORD")
+DB_CONAF_USER = os.getenv("DB_CONAF_USER")
+DB_CONAF_ADDRESS = os.getenv("DB_CONAF_ADDRESS")
+
 # Conectarse a base de datos de administradores PostgreSQL
 engine = create_engine(f"postgresql://postgres:{DB_PASSWORD}@{DB_ADDRESS}/{DB_NAME}")
+
+engine_conaf = create_engine(
+    f"postgresql://{DB_CONAF_USER}:{DB_CONAF_PASSWORD}@{DB_ADDRESS}/{DB_CONAF_NAME}"
+)
 
 
 # Crear la base de datos y las tablas si no existen
---
+++ db.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7486b8c1158f903f43df5c8f68de92ee115b5039cafebc20609eb9a51caa4753
+size 130773792
