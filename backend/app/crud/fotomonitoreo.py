52498e4b6b59f011725d07f2f21cf40def3b407a
Parker <aparkerdf@protonmail.com>
2023-10-04 19:21:22 -0300

    Side menu routes

--- backend/app/crud/fotomonitoreo.py
+++ backend/app/crud/fotomonitoreo.py
@@ -1,4 +1,4 @@
-from asyncpg import Connection
+from asyncpg import Connection, Record
 from fastapi import HTTPException
 
 
@@ -9,4 +9,44 @@ async def get_example(conn: Connection):
         # Process the result here (e.g., print it)
         print(result)
     except Exception as _:
+        raise HTTPException(status_code=500, detail="Error inesperado")
+
+
+async def obtener_regiones(conn: Connection):
+    try:
+        query = 'SELECT Distinct("Nom_region") FROM inputs'
+        result = await conn.fetch(query, record_class=Record)
+        return result
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail="Error inesperado")
+
+
+async def obtener_unidades(region: int, conn: Connection):
+    query = 'SELECT DISTINCT("Unidad"), "Unidad_COD", "Nom_region"  FROM inputs WHERE "Ord_region" = $1'
+    try:
+        result = await conn.fetch(query, region, record_class=Record)
+        return result
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail="Error inesperado")
+
+
+async def obtener_anios(unidad: str, region: int, conn: Connection):
+    query = 'SELECT DISTINCT("Ano") FROM inputs WHERE "Unidad_COD" = $1 AND "Ord_region" = $2'
+    try:
+        result = await conn.fetch(query, unidad, region, record_class=Record)
+        return result
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail="Error inesperado")
+
+
+async def obtener_especies(unidad: str, anio: int, region: int, conn: Connection):
+    query = 'SELECT DISTINCT("Nom_comun") FROM inputs WHERE "Ano" = $1 AND "Unidad_COD" = $2 AND "Ord_region" = $3'
+    try:
+        result = await conn.fetch(query, anio, unidad, region, record_class=Record)
+        return result
+    except Exception as e:
+        print(e)
         raise HTTPException(status_code=500, detail="Error inesperado")
--- backend/app/router/fotomonitoreo.py
+++ backend/app/router/fotomonitoreo.py
@@ -1,11 +1,49 @@
-from asyncpg.connection import asyncpg
-from fastapi import APIRouter, Cookie, Depends, HTTPException, Response
-from pydantic import BaseModel
-from app.crud.fotomonitoreo import get_example
-from asyncpg import Connection
 from app.crud.database import get_session_fotomonitoreo
+from app.crud.fotomonitoreo import (get_example, obtener_anios,
+                                    obtener_especies, obtener_regiones,
+                                    obtener_unidades)
+from asyncpg import Connection
+from fastapi import APIRouter, Depends
+
 router = APIRouter()
 
+
 @router.get("")
-async def idk(conn: Connection = Depends(get_session_fotomonitoreo),):
+async def idk(
+    conn: Connection = Depends(get_session_fotomonitoreo),
+):
     return await get_example(conn)
+
+
+@router.get("/regiones")
+async def get_regiones(conn: Connection = Depends(get_session_fotomonitoreo)):
+    regiones = await obtener_regiones(conn)
+    r = [region["Nom_region"] for region in regiones]
+    return r
+
+
+@router.get("/regiones/{region}")
+async def get_unidades(
+    region: int, conn: Connection = Depends(get_session_fotomonitoreo)
+):
+    rows = await obtener_unidades(region, conn)
+    return rows
+
+
+@router.get("/regiones/{region}/{unidad}")
+async def get_anios(
+    region: int, unidad: str, conn: Connection = Depends(get_session_fotomonitoreo)
+):
+    rows = await obtener_anios(unidad, region, conn)
+    return rows
+
+
+@router.get("/regiones/{region}/{unidad}/{anio}")
+async def get_especies(
+    region: int,
+    unidad: str,
+    anio: int,
+    conn: Connection = Depends(get_session_fotomonitoreo),
+):
+    rows = await obtener_especies(unidad, anio, region, conn)
+    return rows
