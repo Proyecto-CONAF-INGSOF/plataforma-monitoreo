b2a7d1f582b7b5da817d6df2b1717f8d6b866cfc
Agustin Soto <agusoto@alumnos.uai.cl>
2023-10-04 19:49:43 -0300

    restructurando

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,19 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
-import Monitoring from './component/Monitoring';
 import Login from './component/Login';
+import Home from './pages/Home';
+import Header from './component/Navbar';
+import Sidebar from './component/Sidebar';
+
 
 function App() {
   return (
     <Router>
+      <Header/> 
+      <Sidebar/>       
       <Routes>
-        <Route path="/" element={<Monitoring/>} />
+        <Route path="/" element={<Home/>} />
         <Route path="/admin" element={<Login/>} />
         {/* Agrega más rutas para otras páginas */}
       </Routes>
