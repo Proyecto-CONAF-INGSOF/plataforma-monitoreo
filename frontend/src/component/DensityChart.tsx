47a75d7dd9314f8d35f74fb370150cc64ded0363
Diego Leiva Reyes <diegoleivareyes@MacBook-Pro-de-Diego.local>
2023-10-21 14:41:40 -0300

    TICS331-61 <estilos de grafico de densidad listos>

--- frontend/src/component/DensityChart.tsx
+++ frontend/src/component/DensityChart.tsx
@@ -10,30 +10,30 @@ type DensityChartOptions = Highcharts.Options;
 
 const DensityChart = () => {
     const data = [
-        { time: '12 AM', density: 0.1 },
-        { time: '1 AM', density: 0.2 },
-        { time: '2 AM', density: 0.4 },
-        { time: '3 AM', density: 0.5 },
-        { time: '4 AM', density: 0.3 },
-        { time: '5 AM', density: 0.1 },
-        { time: '6 AM', density: 0.2 },
-        { time: '7 AM', density: 0.3 },
-        { time: '8 AM', density: 0.4 },
-        { time: '9 AM', density: 0.5 },
-        { time: '10 AM', density: 0.4 },
-        { time: '11 AM', density: 0.3 },
-        { time: '12 PM', density: 0.2 },
-        { time: '1 PM', density: 0.1 },
-        { time: '2 PM', density: 0.3 },
-        { time: '3 PM', density: 0.4 },
-        { time: '4 PM', density: 0.5 },
-        { time: '5 PM', density: 0.4 },
-        { time: '6 PM', density: 0.3 },
-        { time: '7 PM', density: 0.2 },
-        { time: '8 PM', density: 0.1 },
-        { time: '9 PM', density: 0.2 },
-        { time: '10 PM', density: 0.3 },
-        { time: '11 PM', density: 0.4 },
+        { time: '0 ', density: 0.1 },
+        { time: '1 ', density: 0.2 },
+        { time: '2 ', density: 0.4 },
+        { time: '3 ', density: 0.5 },
+        { time: '4 ', density: 0.3 },
+        { time: '5 ', density: 0.1 },
+        { time: '6 ', density: 0.2 },
+        { time: '7 ', density: 0.3 },
+        { time: '8 ', density: 0.4 },
+        { time: '9 ', density: 0.5 },
+        { time: '10 ', density: 0.4 },
+        { time: '11 ', density: 0.3 },
+        { time: '12 ', density: 0.2 },
+        { time: '13 ', density: 0.1 },
+        { time: '14 ', density: 0.3 },
+        { time: '15 ', density: 0.4 },
+        { time: '16 ', density: 0.5 },
+        { time: '17 ', density: 0.4 },
+        { time: '18 ', density: 0.3 },
+        { time: '19 ', density: 0.2 },
+        { time: '20 ', density: 0.1 },
+        { time: '21 ', density: 0.2 },
+        { time: '22 ', density: 0.3 },
+        { time: '23 ', density: 0.4 },
       ];
 
   const categories = data.map(item => item.time);
@@ -52,14 +52,44 @@ const DensityChart = () => {
     xAxis: {
       categories: categories,
       tickmarkPlacement: 'on',
-      lineWidth: 0,
+      lineWidth: 4, // Ancho de línea del eje X
+      lineColor:'#CED6E9',
+      gridLineColor:'rgba(224,224,224)',
+      labels: {
+        style: {
+          color: 'rgba(100,100,100)', // Color de las etiquetas del eje X
+          fontSize: '12px', // Tamaño de fuente de las etiquetas
+          fontWeight: 'light', // Peso de fuente de las etiquetas
+        },
+      },
     },
     yAxis: {
       min: 0,
       max: 1, // Ajusta el rango máximo según tus datos de densidad
+      tickInterval: 0.09, // Controla la cantidad de gridlines en el eje Y
+      gridLineInterpolation: 'plygon', // Configura las gridlines como rectas
       title: {
+        enabled: false,
         text: 'Densidad',
       },
+      plotBands: [
+        {
+          from: 0, // Valor inicial del nivel
+          to: 0.18, // Valor final del nivel
+          color: 'rgba(249, 249, 249)', // Color del área entre los niveles
+        },
+        {
+          from: 0.36, // Valor inicial del nivel
+          to: 0.54, // Valor final del nivel
+          color: 'rgba(249, 249, 249)', // Color del área entre los niveles
+        },
+        {
+          from: 0.72, // Valor inicial del nivel
+          to: 0.9, // Valor final del nivel
+          color: 'rgba(249, 249, 249)', // Color del área entre los niveles
+        },
+        // Agrega más plotBands según sea necesario
+      ],
     },
     series: [
       {
@@ -72,14 +102,13 @@ const DensityChart = () => {
     ],
     plotOptions: {
       column: {
-        pointPadding: 0,
+        pointPadding: 0.04,
         groupPadding: 0,
-        borderWidth: 0,
+        borderWidth: 0.01,
       },
       series:{
         stacking: 'normal',
         shadow: false,
-        groupPadding: 0,
         pointPlacement: 'on'
       }
     },
@@ -92,7 +121,7 @@ const DensityChart = () => {
   return (
     <div>
       <h2>Gráfico de Densidad Horaria</h2>
-      <div id="density-chart" style={{ width: '400px', height: '400px' }} />
+      <div id="density-chart" style={{ width: '600px', height: '450px' }} />
     </div>
   );
 };
