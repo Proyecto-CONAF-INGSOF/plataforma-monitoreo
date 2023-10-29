5c9311a7f33df437d060ed6a8ddcc817030c91e2
Agustin Soto <agusoto@alumnos.uai.cl>
2023-10-29 00:05:48 -0300

    la cague xd

--- frontend/src/component/BoxPlotOcupacion.tsx
+++ frontend/src/component/BoxPlotOcupacion.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Highcharts from 'highcharts';
 import { Ocupacion } from '../services/ocupacion_sitio';
 
@@ -9,8 +9,15 @@ const BoxPlotOcupacion: React.FC<{
   ocupacion,
   id
 }) => {
+    const [selectedValue, setSelectedValue] = useState<string>('');
+
+    const handleSelectionChange = (value: string) => {
+      setSelectedValue(value);
+      // Realiza cualquier acción adicional según el valor seleccionado
+    };
+
     useEffect(() => {
-      const series: Highcharts.SeriesOptionsType[] = []
+      const series: Highcharts.SeriesOptionsType[] = [];
       ocupacion.map((occ) => {
         series.push(
           {
@@ -36,9 +43,11 @@ const BoxPlotOcupacion: React.FC<{
             ]
           }
         )
-      })
-      const options: Highcharts.Options = {
+      });
 
+      // Puedes usar selectedValue aquí para tomar decisiones basadas en la selección
+
+      const options: Highcharts.Options = {
         chart: {
           type: 'boxplot'
         },
@@ -54,25 +63,30 @@ const BoxPlotOcupacion: React.FC<{
           min: 0,
           max: 100,
         },
-
         title: {
           text: "Ocupación de sitio histórica"
         },
-
         legend: {
           enabled: false
         },
-
-
         series: series
       }
       Highcharts.chart(id, options);
-    }, [ocupacion, id]);
+    }, [ocupacion, id, selectedValue]);
+
     return (
       <div>
         <div id={id} />
+        <div>
+          <h2>Selecciona:</h2>
+          <select value={selectedValue} onChange={(e) => handleSelectionChange(e.target.value)}>
+            <option value="opcion1">Opción 1</option>
+            <option value="opcion2">Opción 2</option>
+            {/* Agrega más opciones según tus necesidades */}
+          </select>
+        </div>
       </div>
     )
-  }
+}
 
 export default BoxPlotOcupacion;
